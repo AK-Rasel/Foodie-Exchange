54f742709103a4adc4d4fd17338b1c8d2519ae69
AK-Rasel <raselak101010@gmail.com>
2024-02-12 01:19:07 +0600

    back end to came data

--- src/Hooks/useMenu.jsx
+++ src/Hooks/useMenu.jsx
@@ -4,7 +4,7 @@ const useMenu = () => {
   const [menu, setMenu] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    fetch("menu.json")
+    fetch("http://localhost:5000/api/v1/food_items")
       .then((res) => res.json())
       .then((data) => {
         setMenu(data);
